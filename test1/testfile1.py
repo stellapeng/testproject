48aeb8d5e55dec0c71d364fa054925a3a929c5de
Stella Peng <speng@wealthsimple.com>
2018-09-05 16:08:22 -0400

    - fix

--- test1/testfile1.py
+++ test1/testfile1.py
@@ -3,5 +3,3 @@ import pandas as pd
 
 yhoo = DataReader("yhoo", "yahoo", datetime.datetime(2007, 1, 1),
     datetime.datetime(2012,1,1))
-
-mavg = yhoo['30_MA_Open'] = pd.stats.moments.rolling_mean(yhoo['Open'], 30)
